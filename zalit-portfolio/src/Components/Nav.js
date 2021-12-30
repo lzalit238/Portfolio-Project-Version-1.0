1bc4f9e2a09f5fc29870283089f60017d3ff016a
lzalit238 <lzalit238@west-mec.org>
2021-12-29 19:37:51 -0700

    Update Portfolio w Router

---
+++ zalit-portfolio/src/Components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Nav = () => {
+    return (
+        <div className="nav">
+        <a href="/">Home</a>
+        <a href="about">About</a>
+        <a href="">Contact</a>
+        <a href="">Resume</a>
+      </div>
+    )
+}
